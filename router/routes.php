02f264e04a3b773be10a8cc55fdb5871d8aa3cbc
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2026-05-12 12:31:41 +0200

    init

---
+++ router/routes.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__.'/router.php';
+
+get('/', 'pages/index.php');
+
+// DYNAMIC ROUTES REFERENCE
+// get('/items/$category', 'pages/items.php');
+// get('/items/$category/size/$size', 'pages/items.php');
+// get("/property", "api/api-get-item.php");
+// get("/property-delete", "api/api-delete-item.php");
